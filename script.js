1813d2a7c798b9a481d24b71b04e2c6cf12ac2c0
Hossam Gamal Elhelw <91392770+HossamGamalElhelw@users.noreply.github.com>
2023-05-11 14:53:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,68 @@ form.addEventListener("submit", function (event) {
     "Sorry We can't receive any message now!\nyou can contact with our team if you want any details.\nthanks!"
   );
 });
+
+///////////////
+// let materials = document.querySelectorAll(".material_1");
+
+// materials.forEach(function (material) {
+//   let materialIcon = material.querySelector(".materialICon");
+//   let materialIcon2 = material.querySelector(".materialICon2");
+//   let materialScreen = material.querySelector(".Material_1Screen");
+//   let materialTitle = material.querySelector(".title_material");
+
+//   materialTitle.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     materialScreen.classList.toggle("hidden_material");
+//     materialScreen.classList.toggle("active_material");
+//     materialIcon.classList.toggle("hidden_material");
+//     materialIcon2.classList.toggle("hidden_material");
+//   });
+// });
+let materials = document.querySelectorAll(".material");
+
+materials.forEach(function (material) {
+  let materialIcon = material.querySelector(".materialICon");
+  let materialIcon2 = material.querySelector(".materialICon2");
+  let materialScreen = material.querySelector(".material_screen");
+  let materialTitle = material.querySelector(".title_material");
+  let materialHeader = material.querySelector(".material_header");
+
+  // materialIcon.addEventListener("click", function (event) {
+  //   event.preventDefault();
+  //   materialScreen.classList.toggle("hidden_material");
+  //   materialScreen.classList.toggle("active_material");
+  //   materialIcon.classList.toggle("hidden_material");
+  //   materialIcon2.classList.toggle("hidden_material");
+  // });
+  materialHeader.addEventListener("click", function (event) {
+    event.preventDefault();
+    materialScreen.classList.toggle("hidden_material");
+    materialScreen.classList.toggle("active_material");
+    materialIcon.classList.toggle("hidden_material");
+    materialIcon2.classList.toggle("hidden_material");
+  });
+
+  let dropLists = material.querySelectorAll(".drop_list");
+
+  dropLists.forEach(function (dropList) {
+    let dropListIcon = dropList.querySelector(".drop_list_icon");
+    let dropListTitle = dropList.querySelector(".drop_list_title");
+    let dropListContent = dropList.querySelector(".drop_list_content");
+
+    dropListTitle.addEventListener("click", function (event) {
+      event.preventDefault();
+      dropListContent.classList.toggle("hidden_material");
+      dropListIcon.classList.toggle("fa-chevron-down");
+      dropListIcon.classList.toggle("fa-chevron-up");
+    });
+  });
+});
+
+// animation
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loading");
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 2000);
+});
